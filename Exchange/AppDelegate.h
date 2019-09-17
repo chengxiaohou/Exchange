a4e6bc3b44dec364867bc771a466f32c0b8cdba7
fy <fengyi@wondershare.cn>
2019-09-17 15:23:09 +0800

    Initial Commit

---
+++ Exchange/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Exchange
+//
+//  Created by fengyi on 2019/9/17.
+//  Copyright © 2019 cxh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Exchange/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Exchange
+//
+//  Created by fengyi on 2019/9/17.
+//  Copyright © 2019 cxh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
